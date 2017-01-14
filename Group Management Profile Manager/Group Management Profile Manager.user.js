db445752d5da94f9527b30075487439a3c3ad0ba
Adam <xadamxk@gmail.com>
2017-01-14 03:45:02 -0600

    update
    Bug fix - Adding/Removing broke in code clean up of v1.1.0

--- Group Management Profile Manager/Group Management Profile Manager.user.js
+++ Group Management Profile Manager/Group Management Profile Manager.user.js
@@ -2,11 +2,11 @@
 // @name       Group Management Profile Manager
 // @author xadamxk
 // @namespace  https://github.com/xadamxk/HF-Scripts
-// @version    1.1.0
+// @version    1.1.1
 // @description  Adds group management buttons to profiles (add/remove) for HF leaders
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
-// @match      *://hackforums.net/member.php?action=profile&uid=*
-// @match      *://hackforums.net/managegroup.php?action=joinrequests&gid=*
+// @match      *://hackforums.net/*
+// @match      *://hackforums.net*
 // @copyright  2016+
 // @iconURL https://raw.githubusercontent.com/xadamxk/HF-Userscripts/master/scripticon.jpg
 // @updateURL https://github.com/xadamxk/HF-Userscripts/raw/master/Group%20Management%20Profile%20Manager/Group%20Management%20Profile%20Manager.user.js
@@ -14,17 +14,27 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ------------------------------ Change Log ----------------------------
+// version 1.1.1: Bug fix - Adding/Removing broke in code clean up of v1.1.0
 // version 1.1.0: Implemented Accept/Ignore/Decline all radio buttons on join request menu, cleaned up code
 // version 1.0.1: Public Release
 // version 1.0.0: Beta Release
 // ==/UserScript==
 // ------------------------------ Dev Notes -----------------------------
 // Add quick deny all?
+// Restructure code to support finding userbars better (if image contains string of group name?)
 // ------------------------------ SETTINGS ------------------------------
 // Key used to store group name,gid,etc. (Don't change)
 const GM_ValAddr = "groupsInfo"; // (Default: 'groupsInfo')
-var declineAllAutomatically = true; // (Default: false)
+// Which select all radio button to default to Options: 'acceptAllRadio','ignoreAllRadio','declineAllRadio'
+var defaultSelectAll = "ignoreAllRadio"; // (Default: ignoreAllRadio)
+// Automatically declines group join requests if any are present
+var declineAllAutomatically = false; // (Default: false)
+// Debug Mode - Print certain results to console
+var debug = true; // (Default: false)
 // ------------------------------ ON PAGE LOAD ------------------------------
+// Global variables
+var uid = $(location).attr('href').replace(/[^0-9]/g, '');
+var username = $("span[class*='group']").text();
 // Profile
 if (window.location.href.includes("hackforums.net/member.php?action=profile&uid="))
     runonProfile();
@@ -50,7 +60,7 @@ function addUser(gid){
         "username": username
     },
            function(data,status){
-        //console.log("Data: " + data + "\nStatus: " + status);
+        if(debug){console.log("Add User\nData: " + data + "\nStatus: " + status);}
         location.reload();
     });
 }
@@ -68,7 +78,7 @@ function removeUser(gid){
         "removeuser[0]": uid
     },
            function(data,status){
-        //console.log("Data: " + data + "\nStatus: " + status);
+        if(debug){console.log("Remove User\nData: " + data + "\nStatus: " + status);}
         location.reload();
     });
 }
@@ -114,11 +124,11 @@ function getGroupInfo(){
             else{
                 $(response).find("a:contains('View Members')").each(function() {
                     groupInfoArray.push($(this).parent().prev().text()); // Group Name
-                    console.log(groupInfoArray[0]);
+                    if(debug){console.log(groupInfoArray[0]);}
                     groupInfoArray.push($(this).attr("href").substr(20)); // Group ID
-                    console.log(groupInfoArray[1]);
+                    if(debug){console.log(groupInfoArray[1]);}
                     groupInfoArray.push($(this).parent().next().text().replace(/[^0-9]/g, '')); // Pending Requests
-                    console.log(groupInfoArray[2]);
+                    if(debug){console.log(groupInfoArray[2]);}
                 });
                 GM_setValue(GM_ValAddr, groupInfoArray.join().toString());
             }
@@ -132,26 +142,11 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-// Mark all radio buttons with col index
-function markAllRadio(colIndex){
-    var joinRequestTable = $(".quick_keys form table tbody");
-    $(joinRequestTable).find("tr").each(function( index ) {
-        if (index === 0 || index === 1){
-            // Don't do anything
-        } else{
-            // Select all radios based on selected -all radio
-            $(joinRequestTable).find("tr:eq("+index+") td:eq("+colIndex+") input").prop("checked", true);
-        }
-    });
-}
-
 function runonProfile(){
     var prevInfo;
-    var uid = $(location).attr('href').replace(/[^0-9]/g, '');
-    var username = $("span[class*='group']").text();
-
     // Check for previous group info
     prevInfo = GM_getValue(GM_ValAddr, false);
+    if(debug){console.log("prevInfo: "+prevInfo);}
     // Grab group info
     if (!prevInfo)
         getGroupInfo();
@@ -180,7 +175,7 @@ function runonJoinRequestMenu(){
             .append("<br>")
             .append($("<input>").attr("id",radioID).attr("type","radio").addClass("radio").attr("name","allRadio"));
     }
-    console.log("'allRadio' labels & radio buttons appened.");
+    if(debug){console.log("'allRadio' labels & radio buttons appened.");}
     $('input[type=radio][name=allRadio]').change(function() {
         console.log($(this).attr("id") + " button checked.");
         switch($(this).attr("id")){
@@ -190,11 +185,37 @@ function runonJoinRequestMenu(){
                 break;
             case "declineAllRadio": markAllRadio(4);
                 break;
-            default : console.log("error");
+            default : console.log("if you're seeing this, my script no longer works.");
+        }
+    });
+}
+
+// Mark all radio buttons with col index
+function markAllRadio(colIndex){
+    var joinRequestTable = $(".quick_keys form table tbody");
+    $(joinRequestTable).find("tr").each(function( index ) {
+        if (index === 0 || index === 1){
+            // Don't do anything
+        } else{
+            // Select all radios based on selected -all radio
+            $(joinRequestTable).find("tr:eq("+index+") td:eq("+colIndex+") input").prop("checked", true);
         }
     });
 }
 
 function runonEveryHFPage(){
-    // every
+    var groupNoticeDiv;
+    // Check for pm alert class
+    if ($(".pm_alert").length > 0){
+        // Check alerts for group notice
+        $( ".pm_alert" ).each(function( index ) {
+            if ($(this).attr("id") === undefined && $(this).find("Group Leader Notice"))
+                groupNoticeDiv = $(this);
+        });
+    }
+    // Group Notice
+    if(groupNoticeDiv !== undefined){
+        if(debug){console.log("Group Notice Found!");}
+        
+    }
 }
