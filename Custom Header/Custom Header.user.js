ac0f05b99ffde31aa54dba6379deb472e6053164
Adam <xadamxk@users.noreply.github.com>
2017-02-10 15:15:20 -0600

    Update Custom Header.user.js

--- Custom Header/Custom Header.user.js
+++ Custom Header/Custom Header.user.js
@@ -2,7 +2,7 @@
 // @name       Custom Header
 // @author xadamxk
 // @namespace  https://github.com/xadamxk/HF-Scripts
-// @version    1.2
+// @version    1.3
 // @description  Adds various shortcuts to the HF toolbar. (Leave strings blank to exclude)
 // @match      *://hackforums.net/*
 // @copyright  2016+
@@ -12,23 +12,23 @@
 var section1_label = "Lounge";
 var section1_fid = "25";
 
-var section2_label = "Legends";
-var section2_fid = "349";
+var section2_label = "RANF";
+var section2_fid = "2";
 
-var section3_label = "Computing Tab";
-var section3_fid = "88";
+var section3_label = "Groups";
+var section3_fid = "53";
 
-var section4_label = "Groups";
-var section4_fid = "53";
+var section4_label = "Web Browsers";
+var section4_fid = "247";
 
-var section5_label = "iOS & iDevices";
-var section5_fid = "137";
+var section5_label = "Eclipse";
+var section5_fid = "250";
 
-var section6_label = "RANF";
-var section6_fid = "2";
+var section6_label = "Market";
+var section6_fid = "105";
 
-var section7_label = "Mentor";
-var section7_fid = "123";
+var section7_label = "PM Tracking";
+var section7_fid = "private.php?action=tracking";
 
 var regex = /\(Unread(.*?)\)/;
 var revised = "(Unread $1) | <a href='forumdisplay.php?fid="+section1_fid+"'>"+section1_label+" </a>"+
@@ -37,6 +37,6 @@ var revised = "(Unread $1) | <a href='forumdisplay.php?fid="+section1_fid+"'>"+s
     "| <a href='forumdisplay.php?fid="+section4_fid+"'>"+section4_label+" </a>"+
     "| <a href='forumdisplay.php?fid="+section5_fid+"'>"+section5_label+" </a>"+
     "| <a href='forumdisplay.php?fid="+section6_fid+"'>"+section6_label+" </a>"+
-    "| <a href='forumdisplay.php?fid="+section7_fid+"'>"+section7_label+" </a>|";
+    "| <a href='"+section7_fid+"'>"+section7_label+" </a>|";
 document.getElementById('panel').innerHTML= document.getElementById('panel').innerHTML.replace(regex,revised);
 
