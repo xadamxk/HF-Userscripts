6f7d31c488884c30b73ee52fdf400309910a4d7c
Adam <xadamxk@gmail.com>
2017-02-11 13:25:35 -0600

    update
    Public Release

--- Small Scripts/OriginalAdditionalUsergroups.user.js
+++ Small Scripts/OriginalAdditionalUsergroups.user.js
@@ -2,16 +2,17 @@
 // @name       Original Additional Usergroups
 // @author xadamxk
 // @namespace  https://github.com/xadamxk/HF-Scripts
-// @version    1.0.0
+// @version    1.0.1
 // @description  Adds line breaks after each additional usergroup on profiles
 // @require https://code.jquery.com/jquery-3.1.1.js
 // @match      *://hackforums.net/member.php?action=profile&uid=*
 // @copyright  2016+
-// @updateURL
-// @downloadURL
+// @updateURL https://github.com/xadamxk/HF-Userscripts/raw/master/Small%20Scripts/OriginalAdditionalUsergroups.user.js
+// @downloadURL https://github.com/xadamxk/HF-Userscripts/raw/master/Small%20Scripts/OriginalAdditionalUsergroups.user.js
 // @iconURL https://raw.githubusercontent.com/xadamxk/HF-Userscripts/master/scripticon.jpg
 // ==/UserScript==
 // ------------------------------ Change Log ----------------------------
+// version 1.0.1: Public Release
 // version 1.0.0: Initial Release
 // ------------------------------ Dev Notes -----------------------------
 //
