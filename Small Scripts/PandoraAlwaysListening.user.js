e059679d1df8dea1fd3703106c96bef3ee7a0ac9
Adam <xadamxk@gmail.com>
2018-04-27 19:01:24 -0500

    PandoraAlwaysListening Update

--- Small Scripts/PandoraAlwaysListening.user.js
+++ Small Scripts/PandoraAlwaysListening.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
-// @name         Pandora: Always Listening
-// @namespace    https://github.com/xadamxk/HF-Scripts
-// @version      0.0.1
-// @description  Automatically enable the 'still listening' feature on Pandora
-// @author       xadamxk
+// @name        Pandora: Always Listening
+// @namespace   https://github.com/xadamxk/HF-Scripts
+// @version     0.0.2
+// @description Automatically enable the 'still listening' feature on Pandora
+// @author      xadamxk
 // @require     https://code.jquery.com/jquery-3.1.1.js
 // @include     http://*pandora.com/*
 // @include     https://*pandora.com/*
-// @grant        none
+// @grant       none
+// @copyright   2016+
+// @updateURL   https://github.com/xadamxk/HF-Userscripts/raw/master/Small%20Scripts/PandoraAlwaysListening.user.js
+// @downloadURL https://github.com/xadamxk/HF-Userscripts/raw/master/Small%20Scripts/PandoraAlwaysListening.user.js
 // ==/UserScript==
 // Credit to original author: https://greasyfork.org/en/scripts/1816-pandora-freemium/code
 
