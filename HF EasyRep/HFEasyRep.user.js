4c871c82b2b552e52b8633aa0121c08e13e4025d
Adam <xadamxk@gmail.com>
2018-06-12 00:07:57 -0500

    HFEasyRep Public Release

--- HF EasyRep/HFEasyRep.user.js
+++ HF EasyRep/HFEasyRep.user.js
@@ -2,23 +2,23 @@
 // @name       HF EasyRep
 // @author xadamxk
 // @namespace  https://github.com/xadamxk/HF-Scripts
-// @version    1.0.0
+// @version    1.0.1
 // @description Makes giving away reps super easy
 // @require https://code.jquery.com/jquery-3.1.1.js
 // @match      *://hackforums.net/reputation.php?uid=*
 // @match      *://hackforums.net/showthread.php?tid=*
-// @copyright  2016+
-// @updateURL
-// @downloadURL
+// @copyright  2018+
+// @updateURL https://github.com/xadamxk/HF-Userscripts/raw/master/HF%20EasyRep/HFEasyRep.user.js
+// @downloadURL https://github.com/xadamxk/HF-Userscripts/raw/master/HF%20EasyRep/HFEasyRep.user.js
 // @iconURL https://raw.githubusercontent.com/xadamxk/HF-Userscripts/master/scripticon.jpg
 // ==/UserScript==
 // ------------------------------ Change Log ----------------------------
 // version 1.0.1: Public Release
 // version 1.0.0: Beta Release
 // ------------------------------ Dev Notes -----------------------------
-//
+// If you like this script, test it out on me :)
 // ------------------------------ SETTINGS ------------------------------
-//
+// None atm
 // ------------------------------ Script ------------------------------
 // Ripped from HFX
 if ($(".posts").length > 0){
@@ -42,12 +42,9 @@ if ($(".posts").length > 0){
                                                .addClass("bitButton"));
     });
 } else if ($(".postbit_report").length > 0){
-    console.log("test");
     $(".postbit_report").each(function (index) {
         var usernameUID = $(this).parent().next().attr("href").replace(/\D/g, '');
-        console.log(usernameUID);
         var usernameName = $(this).parent().next().children().text();
-        console.log(usernameName);
         $(this).prepend($("<a>").append("<span>").attr({
             "title": "Quick Rep " + usernameName,
             "onclick": "MyBB.reputation("+usernameUID+"); return false;",
