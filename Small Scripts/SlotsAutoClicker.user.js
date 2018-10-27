5fb02c3d2c84b6fb1997dc67cc049b873606a0f8
Adam <xadamxk@gmail.com>
2018-10-27 18:35:12 -0500

    Update Small Scripts/SlotsAutoClicker.user.js

---
+++ Small Scripts/SlotsAutoClicker.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name       HF Slot AutoClicker
+// @author xadamxk
+// @namespace  https://github.com/xadamxk/HF-Scripts
+// @version    1.0.0
+// @description Autoclick the slots button every second
+// @require https://code.jquery.com/jquery-3.1.1.js
+// @match      *://hackforums.net/slots.php
+// @copyright  2018+
+// ==/UserScript==
+var byteLimit = 1000;
+
+var iteration = 0;
+(function loop() {
+    setTimeout(function () {
+        if (parseInt($("#credits").text()) > byteLimit){
+            iteration++;
+            console.clear();
+            console.log("Iteration " + iteration);
+            $("#spinButton").click();
+        } else {
+            console.log("Credits too low");
+        }
+        loop()
+    }, 2000);
+}());
