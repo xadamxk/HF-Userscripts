a22f0a7d25dafd35dd8e8c6d0bff24010082eb6d
Adam <xadamxk@gmail.com>
2019-05-27 15:06:15 -0500

    Update HFG.user.js
    Disabled low reward jobs, added leaderboard/log shortcuts, formatted file

--- HF Game/HFG.user.js
+++ HF Game/HFG.user.js
@@ -4,11 +4,13 @@
 // @namespace  https://github.com/xadamxk/HF-Scripts
 // @version    1.0.0
 // @description Adds various helpful tools to HF's Game (battery indicator & recharge time)
-// @require https://code.jquery.com/jquery-3.1.1.js
-// @require https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js
+// @require     https://code.jquery.com/jquery-3.1.1.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js
 // @match      *://hackforums.net/gamecp.php
 // @match      *://hackforums.net/gamecp.php?action=smallhacks*
 // @match      *://hackforums.net/gamecp.php?action=battery*
+// @match      *://hackforums.net/gamecp.php?action=logs*
+// @match      *://https://hackforums.net/gamecp.php?action=leaderboard&type=1*
 // @copyright  2016+
 // ==/UserScript==
 // ------------------------------ Dev Notes -----------------------------
@@ -16,16 +18,16 @@
 // ------------------------------ SETTINGS ------------------------------
 var debug = false;
 const jobs = {
-    socialEngineerMcDonaldsLunch: "#job_id_11", // 93%
-    facebookPassword: "#job_id_12",             // 86%
-    downloadTorrentMSWindows: "#job_id_13",     // 79%
-    makeStealthPaypal: "#job_id_14",            // 72%
-    ewhoreRichDude: "#job_id_15",               // 65%
-    doxSomeoneTwitter: "#job_id_16",            // 58%
-    amazonPhishingPage: "#job_id_17",           // 51%
-    accountGeneratorNetflix: "#job_id_18",      // 44%
-    infectCoworkerVirus: "#job_id_19",          // 37%
-    hackNeighborWifi: "#job_id_20",             // 30%
+    //     socialEngineerMcDonaldsLunch: "#job_id_11", // 93%
+    //     facebookPassword: "#job_id_12",             // 86%
+    //     downloadTorrentMSWindows: "#job_id_13",     // 79%
+    //     makeStealthPaypal: "#job_id_14",            // 72%
+    //     ewhoreRichDude: "#job_id_15",               // 65%
+    //     doxSomeoneTwitter: "#job_id_16",            // 58%
+    //     amazonPhishingPage: "#job_id_17",           // 51%
+    //     accountGeneratorNetflix: "#job_id_18",      // 44%
+    //     infectCoworkerVirus: "#job_id_19",          // 37%
+    //     hackNeighborWifi: "#job_id_20",             // 30%
     sqlInjectWebsite: "#job_id_21",             // 23%
     spreadBTCMiner: "#job_id_22",               // 16%
     hackNasaMoonLanding: "#job_id_23"           //  9%
@@ -39,38 +41,71 @@ var isLocked = ($("img[src$='/game/fbi-interrogating.jpg']").length > 0 ? true :
 var isPowerCenter = ($("#progress-bar-percentage").length > 0 ? true : false);
 var batteryPercent = (
     $(".game-top-right-icons > a:eq(4)") ?
-    parseInt($(".game-top-right-icons > a:eq(4)").attr('title').replace('%','')) :
-    0);
+        parseInt($(".game-top-right-icons > a:eq(4)").attr('title').replace('%', '')) :
+        0);
 var date = new Date();
 var localDate = date.toLocaleTimeString();
+// Append Logs & Leaderboard shortcuts
+$(".game-top-right-icons")
+    .prepend($("<a>").attr("href", "gamecp.php?action=logs")
+        .append($("<i>").addClass("hficon-drawer-paper2")
+            .attr({ "title": "Logs" })
+            .css({
+                "margin-left": "12px",
+                "font-size": "24px",
+                "color": "#ababab"
+            })
+            .hover(
+                function () {
+                    $(this).css({ "color": "#4d2f5d" });
+                }, function () {
+                    $(this).css({ "color": "#ababab" })
+                })
+        ))
+    .prepend($("<a>").attr("href", "gamecp.php?action=leaderboard&type=1")
+        .append($("<i>").addClass("hficon-medal-empty")
+            .attr({ "title": "Leaderboard" })
+            .css({
+                "margin-left": "12px",
+                "font-size": "24px",
+                "color": "#ababab"
+            })
+            .hover(
+                function () {
+                    $(this).css({ "color": "#4d2f5d" });
+                }, function () {
+                    $(this).css({ "color": "#ababab" })
+                })
+        )
+    );
 // Append game status info
 $(".game_nav_content_system_container").parent().parent()
     .after($("<tr>")
-           .append($("<td>").addClass("trow1")
-                   .append($("<div>").attr("id","gameStatusContainer")
-                           .append("<b>Cooldown:</b> " + isCooldown.toString().toUpperCase())
-                           .append("<br>")
-                           .append("<b>Locked:</b> " + isLocked.toString().toUpperCase())
-                           .append("<br>")
-                          )));
+        .append($("<td>").addClass("trow1")
+            .append($("<div>").attr("id", "gameStatusContainer")
+                .append("<b>Cooldown:</b> " + isCooldown.toString().toUpperCase())
+                .append("<br>")
+                .append("<b>Locked:</b> " + isLocked.toString().toUpperCase())
+                .append("<br>")
+            )));
 // Append battery info
-var batteryElement = $(".game-top-right-icons :last-child");
-batteryElement.after($("<span>").css({"margin-left":"10px", "color": batteryElement.css('color')}).text(batteryPercent + "%"));
+var batteryElement = $('[class^="hficon-battery-"]');
+batteryElement.after($("<span>").css({ "margin-left": "10px", "color": batteryElement.css('color') }).text(batteryPercent + "%"));
 // Determine game status
-if (batteryPercent < batteryThreshold && !isPowerCenter){
+if (batteryPercent < batteryThreshold && !isPowerCenter) {
     console.log("Battery threshold hit. Navigating to battery...");
     naviagteToBattery();
-} else if (isPowerCenter){
+} else if (isPowerCenter) {
     // Full recharge time
     var extractedRechargeTime = (
         $("#game_content_currentpage").find(".tinytext > span").attr('title') ?
-        $("#game_content_currentpage").find(".tinytext > span").attr('title') :
-        $("#game_content_currentpage").find("em").text()
+            $("#game_content_currentpage").find(".tinytext > span").attr('title') :
+            $("#game_content_currentpage").find("em").text()
     );
     var batteryTimeElement = (
         $("#game_content_currentpage").find(".tinytext > span") ?
-        $("#game_content_currentpage").find(".tinytext > span") :
-        $(".gmiddle").parent().find("em")
+            $("#game_content_currentpage").find(".tinytext > span") :
+            $(".gmiddle").parent().find("em")
     );
     var rechargeDate = moment(extractedRechargeTime, "MMMM Do, YYYY, hh:mm A"); // May 13th, 2019, 02:45 AM
     var differenceInMS = Math.abs(moment().diff(rechargeDate, 'milliseconds')); // Diff in MS from now until recharge time
@@ -82,7 +117,7 @@ else if (isCooldown) {
     // TODO: Add logic to check for page as well?
     runOnCooldown();
 }
-else if (isLocked){
+else if (isLocked) {
     // Grab time from FBI page (are there other pages?)
     $("#gameStatusContainer").append("Reload Time: " + localDate + " + (" + intervalVariableHigh + ")");
     setInterval(reloadPage, intervalVariableHigh);
@@ -91,7 +126,7 @@ else if (isLocked){
     runOnGame();
 }
 
-function runOnCooldown(){
+function runOnCooldown() {
     // Get timer count
     var timer = $(".game_nav_content_system_container > a:eq(2)").find('.game-nav-countdown > span').text();
     var timerMins = parseInt(timer.split(':')[0]);
@@ -104,32 +139,32 @@ function runOnCooldown(){
     setInterval(reloadPage, timerMS);
 }
 
-function runOnGame(){
+function runOnGame() {
     $(selectRandomHackJob()).click();
     $("button:contains('Attempt Hack')").click();
 }
 
-function reloadPage(){
+function reloadPage() {
     console.log("reloaded page?");
     location.reload();
 }
 
-function naviagteToGame(){
+function naviagteToGame() {
     window.location.href = 'https://hackforums.net/gamecp.php?action=smallhacks#lgv';
 }
 
-function naviagteToBattery(){
+function naviagteToBattery() {
     window.location.href = 'https://hackforums.net/gamecp.php?action=battery#lgv';
 }
 
-function calculateRandomInterval(){
+function calculateRandomInterval() {
     return Math.floor(Math.random() * intervalVariableHigh) + intervalVariableLow;
 }
 
 function msToTime(duration) {
-    var milliseconds = parseInt((duration%1000)/100)
-    , seconds = parseInt((duration/1000)%60)
-    , minutes = parseInt((duration/(1000*60))%60);
+    var milliseconds = parseInt((duration % 1000) / 100)
+        , seconds = parseInt((duration / 1000) % 60)
+        , minutes = parseInt((duration / (1000 * 60)) % 60);
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
     return minutes + ":" + seconds + "." + milliseconds;
@@ -137,5 +172,5 @@ function msToTime(duration) {
 
 function selectRandomHackJob(obj) {
     var keys = Object.keys(jobs)
-    return jobs[keys[ keys.length * Math.random() << 0]];
+    return jobs[keys[keys.length * Math.random() << 0]];
 };
