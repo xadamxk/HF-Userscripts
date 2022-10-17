7610b6fcc7eabe217b8a258502b804cdbde237b3
Adam Koewler <xadamxk@gmail.com>
2022-10-17 02:22:26 -0500

    Update HFX Mobile.user.js

--- HFX Mobile/HFX Mobile.user.js
+++ HFX Mobile/HFX Mobile.user.js
@@ -37,7 +37,6 @@
 // Theme changer (accent color + mosaic + logo)
 // ------------------------------ SETTINGS ------------------------------
 const debug = false;
-const maxMobileScreenWidth = 530;
 // ------------------------------ SCRIPT ------------------------------
 const currentUrl = window.location.href;
 dPrint(`Current URL: ${currentUrl}`);
@@ -213,7 +212,7 @@ function findPageMatch(term) {
 }
 // ------------------------------ FUNCTIONS: CompactPosts ------------------------------
 function injectCompactPosts() {
-    if (screen.width > maxMobileScreenWidth) return; // Minimum screen width to trigger script
+    if (screen.width > 530) return; // Minimum screen width to trigger script
 
     const posts = document.getElementsByClassName('post');
     for (const post of posts) {
@@ -606,7 +605,7 @@ function reformatDumbDate(dateStr) {
 
 };
 function injectMobileForumDisplay() {
-    if (screen.width > maxMobileScreenWidth) return; // Minimum screen width to trigger script
+    if (screen.width > 768) return; // Minimum screen width to trigger script
 
     const content = document.querySelector("#content");
     const threadRows = content.querySelectorAll("tr.inline_row");
